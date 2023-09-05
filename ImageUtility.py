798e4608965d0a4fc51935044e888fc673e0714a
Brennan-Cox <bhcox1@radford.edu>
2023-09-05 15:19:50 -0400

    added changes

--- ImageUtility.py
+++ ImageUtility.py
@@ -132,13 +132,13 @@ def relation_Figure(comparison_Set, image, answer, transformations, classified, 
     fig.subplots_adjust(top=3.0, hspace=0)
     
     axs[rows - 1, 0].set_title(title)
-    display_Set(axs[rows - 1, 0], comparison_Set)
+    display_Set(axs[rows - 1, 0], transformations)
     axs[rows - 1, 0].set_title(title)
     axs[rows - 1, 1].remove()
 
     axs[0, 0].set_title("Comparison Set")
     display_Set(axs[0, 0], comparison_Set)
-    
+
     axs[0, 1].set_title("Image to identify {}".format(answer))
     axs[0, 1].imshow(image, cmap="gray")
     
